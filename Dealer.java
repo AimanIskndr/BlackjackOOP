518c951328f6dc7562981d30a0b0e464fdec3869
Aiman Iskandar <esxkandar14@gmail.com>
2023-05-26 11:27:18 +0800

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,26 +1,48 @@
 package application;
 
 public class Dealer extends Player {
-
-	Dealer () {
-		super();
-	}
 	
-	@Override
-	public void takeCard(Deck deck) {
-		
-        while (handSum < 17) {
-        	
-        	Card drawnCard = deck.draw();
-            
-            if (drawnCard != null) {
-            	
-                hand[numOfCards] = drawnCard;
-                numOfCards++;
-                if(drawnCard.isAce()) aceCount++;
-                updateHandSum(drawnCard);
-            }
+	boolean hide = true;
+	
+    Dealer() {
+        super();
+    }
+
+    @Override
+    public void takeCard(Deck deck) {
+        takeCard(deck, true);
+    }
+
+    public void takeCard(Deck deck, boolean auto) {
+        if (auto) {
+            super.takeCard(deck);
+        } 
+        
+        else {
+            do {
+                Card drawnCard = deck.draw();
+
+                if (drawnCard != null) {
+                    hand[numOfCards] = drawnCard;
+                    numOfCards++;
+                    if (drawnCard.isAce()) {
+                        aceCount++;
+                    }
+                    updateHandSum(drawnCard);
+                }
+            } while (handSum < 17);
         }
     }
+	
+    @Override
+    public String getHandSumStr() {
+        if (hide) {
+            return getHiddenHandSum();
+        }
+        return String.valueOf(handSum);
+    }
     
+    public String getHiddenHandSum() {
+    	return hand[0].getValue() + " + ?";
+    }
 }
