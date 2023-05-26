440aafbc76777d42cdbc336f9c3e5e3b78b499fc
Aiman Iskandar <esxkandar14@gmail.com>
2023-05-26 11:27:49 +0800

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,44 +1,39 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
 	
-    private Card[] cards;
+    private List<Card> cards;
     private int currentCount;
 
     public Deck() {
-        cards = new Card[52];
-        currentCount = 0;
+        this.cards = new ArrayList<>();
         initializeDeck();
+        shuffle();
     }
 
     private void initializeDeck() {
-        int index = 0;
         String[] suits = {"Diamond", "Heart", "Club", "Spade"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
         for (String suit : suits) {
             for (String rank : ranks) {
-                cards[index] = new Card(suit, rank);
-                index++;
+                cards.add(new Card(suit, rank));
             }
         }
         currentCount = 52;
     }
 
     public void shuffle() {
-        for (int i = 0; i < 52; i++) {
-            int j = (int) (Math.random() * 51);
-            Card temp = cards[i];
-            cards[i] = cards[j];
-            cards[j] = temp;
-        }
+    	Collections.shuffle(cards);
     }
 
     public Card draw() {
-        if (currentCount == 0) {
-            return null; // Deck is empty
-        }
-        currentCount--;
-        return cards[currentCount];
+    	currentCount--;
+        Card drawnCard = cards.get(currentCount);
+        return drawnCard;
     }
 }
