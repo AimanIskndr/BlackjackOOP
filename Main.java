b48721316a5e30cbea63dc2b11f61deba80875ec
Aiman Iskandar <esxkandar14@gmail.com>
2023-05-28 16:18:22 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,115 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
+    Deck deck = new Deck();
+    Player player = new Player();
+    Dealer dealer = new Dealer();
+
     public void start(Stage primaryStage) {
-    	
-        Card cardS1 = new Card("Clover", "Jack");
-        Card cardH2 = new Card("Spade", "Ace");
-        Card unknown = new Card("Diamond", "8");
 
-        ImageView civ1 = new ImageView(cardS1.getFrontImage());
-        ImageView civ2 = new ImageView(cardH2.getFrontImage());
-        ImageView civ3 = new ImageView(unknown.getBackimage());
+        Pane root = new Pane();
+        root.setPadding(new Insets(10));
 
-        civ1.setFitWidth(100);
-        civ1.setFitHeight(140); 
+        HBox dealerCardsContainer = new HBox(10);
+        dealerCardsContainer.setTranslateX(15);
+        dealerCardsContainer.setTranslateY(70);
 
-        civ2.setFitWidth(100);
-        civ2.setFitHeight(140); 
-        
-        civ3.setFitWidth(100);
-        civ3.setFitHeight(140); 
+        HBox playerCardsContainer = new HBox(10);
+        playerCardsContainer.setTranslateX(15);
+        playerCardsContainer.setTranslateY(420);
 
-        HBox cardsContainer = new HBox(10); 
-        cardsContainer.getChildren().addAll(civ1, civ2, civ3);
+        dealInitialCards();
+        displayPlayerCards(playerCardsContainer);
+        displayDealerCards(dealerCardsContainer);
 
-        BorderPane root = new BorderPane();
-        root.setPadding(new Insets(10)); 
-        root.setTop(cardsContainer);
+        HBox dealerCount = createCountBox("Dealer: ", dealer.getHandSumStr());
+        dealerCount.setAlignment(Pos.TOP_LEFT);
+        dealerCount.setTranslateX(15);
+        dealerCount.setTranslateY(15);
 
-        Scene scene = new Scene(root, 1280, 653, Color.STEELBLUE);
+        HBox playerCount = createCountBox("Player: ", player.getHandSumStr());
+        playerCount.setAlignment(Pos.BOTTOM_LEFT);
+        playerCount.setTranslateX(15);
+        playerCount.setTranslateY(585);
+
+        root.getChildren().addAll(dealerCount, playerCount, dealerCardsContainer, playerCardsContainer);
+
+        Scene scene = new Scene(root, 1120, 630, Color.STEELBLUE);
 
         primaryStage.setTitle("Blackjack");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
-    public static void main(String[] args) {
-        launch(args);
+
+
+    class HitHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent e) {
+            player.takeCard(deck);
+            createCountBox("Player: ", player.getHandSumStr());
+        }
+    }
+
+    private HBox createCountBox(String labelPrefix, String handSumStr) {
+
+        HBox countBox = new HBox(10);
+
+        Text label = new Text(labelPrefix);
+        label.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        label.setFill(Color.WHITE);
+        Text countText = new Text(handSumStr);
+        countText.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        countText.setFill(Color.WHITE);
+        countBox.getChildren().addAll(label, countText);
+
+        return countBox;
+    }
+
+    private void dealInitialCards() {
+
+        player.takeCard(deck);
+        dealer.takeCard(deck);
+        player.takeCard(deck);
+        dealer.takeCard(deck);
+        dealer.hand[1].flip();
+    }
+
+    private void displayPlayerCards(HBox cardsContainer) {
+
+        for (int i = 0; i < player.getNumOfCards(); i++) {
+            Card card = player.hand[i];
+            ImageView imageView = new ImageView(card.getCardImage());
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(140);
+            cardsContainer.getChildren().add(imageView);
+        }
+    }
+
+    private void displayDealerCards(HBox cardsContainer) {
+
+        for (int i = 0; i < dealer.getNumOfCards(); i++) {
+            Card card = dealer.hand[i];
+            ImageView imageView = new ImageView(card.getCardImage());
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(140);
+            cardsContainer.getChildren().add(imageView);
+        }
     }
 }
