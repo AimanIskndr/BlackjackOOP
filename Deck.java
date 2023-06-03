23b93e1e0b1985cc83739f67f7cc69cd3d984ee4
Aizuddin Ishqal <117440383+Juddinnn@users.noreply.github.com>
2023-06-03 19:11:29 +0800

    Arraylist to array
    Forgot index when changing from arraylist to array

--- Deck.java
+++ Deck.java
@@ -1,39 +1,45 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Deck {
-	
-    private List<Card> cards;
-    private int currentCount;
 
-    public Deck() {
-        this.cards = new ArrayList<>();
-        initializeDeck();
-        shuffle();
-    }
+	private Card[] cards;
+	private int currentCount;
 
-    private void initializeDeck() {
-        String[] suits = {"Diamond", "Heart", "Club", "Spade"};
-        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+	public Deck() {
+		cards = new Card[52];
+		currentCount = 0;
+		initializeDeck();
+		shuffle();
+	}
 
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                cards.add(new Card(suit, rank));
-            }
-        }
-        currentCount = 52;
-    }
+	private void initializeDeck() {
+		int index = 0;
+		String[] suits = { "Diamond", "Heart", "Club", "Spade" };
+		String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
-    public void shuffle() {
-    	Collections.shuffle(cards);
-    }
+		for (String suit : suits) {
+			for (String rank : ranks) {
+				cards[index] = new Card(suit, rank);
+				index++;
+			}
+		}
+		currentCount = 52;
+	}
 
-    public Card draw() {
-    	currentCount--;
-        Card drawnCard = cards.get(currentCount);
-        return drawnCard;
-    }
+	public void shuffle() {
+		for (int i = 0; i < 52; i++) {
+			int j = (int) (Math.random() * 51);
+			Card temp = cards[i];
+			cards[i] = cards[j];
+			cards[j] = temp;
+		}
+	}
+
+	public Card draw() {
+		if (currentCount == 0) {
+			return null; // Deck is empty
+		}
+		currentCount--;
+		return cards[currentCount];
+	}
 }
