3fa4ebfcb5124671e56d41535281b0b41bd04de0
Aiman Iskandar <esxkandar14@gmail.com>
2023-06-21 17:08:03 +0800

    added softHand indicator

--- Player.java
+++ Player.java
@@ -5,6 +5,7 @@ public class Player {
     protected int handSum = 0;
     protected int aceCount = 0;
     protected int numOfCards = 0;
+    protected boolean softHand = false;
     Card[] hand = new Card[11];
     
     Player() {}
@@ -14,24 +15,34 @@ public class Player {
         Card drawnCard = deck.draw();
         
         if (drawnCard != null) {
+        	
             hand[numOfCards] = drawnCard;
             numOfCards++;
-            if(drawnCard.isAce()) aceCount++;
+            
+            if(drawnCard.isAce()) {
+            	aceCount++;
+            	softHand = true;
+            }
             updateHandSum(drawnCard);
         }
     }
 
     protected void updateHandSum(Card card) {
-        
+    	
     	handSum += card.getValue();
     	
-    	if(handSum > 21 && aceCount > 0)
+    	if(handSum > 21 && softHand)
     		updateHandSum();
     }
     
     protected void updateHandSum() {
+    	
     	handSum -= 10;
 		aceCount--;
+		
+		if(aceCount == 0) {
+			softHand = false;
+		}
     }
 
     public int getHandSum() {
@@ -39,7 +50,7 @@ public class Player {
 	}
     
     public String getHandSumStr() {
-        return String.valueOf(handSum);
+        return String.valueOf(handSum) + (softHand ? "*" : "");
     }
 
     public int getNumOfCards() {
