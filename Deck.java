98f6dffe84855d16ff0eac43c6460decc313b101
Aiman Iskandar <esxkandar14@gmail.com>
2023-06-24 14:20:12 +0800

    added documentation for Deck.java

--- Deck.java
+++ Deck.java
@@ -1,22 +1,31 @@
 package application;
 
+/**
+ * The Deck class represents a deck of playing cards.
+ */
 public class Deck {
-	
+
     private Card[] cards;
     private int currentCount;
 
+    /**
+     * Constructs a new Deck object and initializes it with a standard deck of 52 playing cards.
+     */
     public Deck() {
         cards = new Card[52];
         initializeDeck();
         shuffle();
     }
 
+    /**
+     * Initializes the deck with a standard set of 52 playing cards.
+     */
     private void initializeDeck() {
-        String[] suits = {"Diamond", "Heart", "Club", "Spade"};
+        String[] suits = {"Diamonds", "Hearts", "Clubs", "Spades"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-        
+
         currentCount = 0;
-        
+
         for (String suit : suits) {
             for (String rank : ranks) {
                 cards[currentCount] = new Card(suit, rank);
@@ -25,16 +34,23 @@ public class Deck {
         }
     }
 
-    public void shuffle(/*Monkey sort*/) {
-    	Card temp;
-    	for (int i = 0; i < 52; i++) {
-            int j = (int) (Math.random() * 51);
+    /**
+     * Shuffles the cards in the deck using the monkey sort algorithm.
+     */
+    public void shuffle() {
+        Card temp;
+        for (int i = 0; i < 52; i++) {
+            int j = (int) (Math.random() * 52);
             temp = cards[i];
             cards[i] = cards[j];
             cards[j] = temp;
         }
     }
 
+    /**
+     * Draws a card from the deck.
+     * @return the Card object drawn from the deck, or null if the deck is empty
+     */
     public Card draw() {
         if (currentCount == 0) {
             return null;
